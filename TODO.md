72e1ba9e4800f03996b976b599ad7629465226e5
Eric Pardee <github@ericpardee.com>
2023-10-16 23:06:32 -0700

    add more comments

--- TODO.md
+++ TODO.md
@@ -1,7 +1,4 @@
 # TODO
 
 - [ ] Update YNAB Category for known merchants
-- [ ] Add error Handling
-- [ ] Add more Documentation
-- [ ] Add performance optimizations to handle a large number of transactions
 - [ ] Add tests
--- app.py
+++ app.py
@@ -4,35 +4,51 @@ import sys
 from datetime import datetime, timedelta
 
 # Constants
+# API endpoints for Privacy.com and YNAB
 PRIVACY_API_ENDPOINT = "https://api.privacy.com/v1/"
 YNAB_API_ENDPOINT = "https://api.youneedabudget.com/v1/"
+
+# Get authentication tokens and budget ID from environment variables or default values
 PRIVACY_AUTH_TOKEN = os.environ.get('PRIVACY_API_TOKEN') or "PRIVACY_API_TOKEN"
 YNAB_AUTH_TOKEN = os.environ.get('YNAB_API_TOKEN') or "YNAB_API_TOKEN"
 YNAB_BUDGET_ID = os.environ.get('YNAB_BUDGET_ID') or "YNAB_BUDGET_ID"
+
+# Debug flag for verbose output
 DEBUG = os.environ.get('DEBUG', 'false').lower() == 'true'
-PRIVACY_DESCRIPTOR = os.environ.get('PRIVACY_DESCRIPTOR', 'Pwp*privacy.com') # how privacy.com transactions appear in YNAB
-PRIVACY_PAGE_SIZE = int(os.environ.get('PRIVACY_PAGE_SIZE', '50')) # how many transactions to fetch from privacy.com at a time
+
+# Descriptor to identify Privacy.com transactions in YNAB
+PRIVACY_DESCRIPTOR = os.environ.get('PRIVACY_DESCRIPTOR', 'Pwp*privacy.com')
+
+# Setting the number of transactions to fetch from privacy.com at once
+PRIVACY_PAGE_SIZE = int(os.environ.get('PRIVACY_PAGE_SIZE', '50'))
 
 def debug_print(*args, **kwargs):
-    """Prints only if DEBUG flag is set."""
+    """Utility function to print debug messages if the DEBUG flag is set."""
     if DEBUG:
         print(*args, **kwargs, end='\n\n')
 
 def ynab_to_privacy_amount(ynab_amount):
-    """Convert YNAB's milliunit format to Privacy.com's integer representation."""
+    """
+    Convert YNAB's milliunit format to Privacy.com's integer representation.
+    For example, YNAB represents $71.88 outflow as -71880, which is converted to 7188 for Privacy.com.
+    """
     return abs(int(ynab_amount)) // 10
 
-# Connect to YNAB and fetch transactions matching the privacy descriptor
 def get_ynab_transactions():
+    """
+    Fetch transactions from YNAB that match the PRIVACY_DESCRIPTOR.
+    
+    Returns:
+        List of transactions that match the PRIVACY_DESCRIPTOR.
+    """
     headers = {
         "Authorization": f"Bearer {YNAB_AUTH_TOKEN}",
         "Content-Type": "application/json"
     }
     try:
         response = requests.get(f"{YNAB_API_ENDPOINT}budgets/{YNAB_BUDGET_ID}/transactions", headers=headers)
-        response.raise_for_status()  # Raise an error if the response contains an HTTP error status
+        response.raise_for_status()
         data = response.json()
-
         privacy_transactions = [txn for txn in data["data"]["transactions"] if PRIVACY_DESCRIPTOR in txn["payee_name"] and txn["memo"] in [None, ""]]
         debug_print("YNAB privacy transactions:\n", privacy_transactions)
         return privacy_transactions
@@ -41,17 +57,25 @@ def get_ynab_transactions():
         sys.exit(1)
 
 def fetch_privacy_transactions(start_date, end_date):
+    """
+    Fetch transactions from Privacy.com within a specified date range.
+
+    Args:
+        start_date: Start date for transaction range.
+        end_date: End date for transaction range.
+
+    Returns:
+        List of transactions from Privacy.com within the specified date range.
+    """
     headers = {
         "Authorization": f"api-key {PRIVACY_AUTH_TOKEN}",
         "Accept": "application/json"
     }
-    
     begin_date = start_date.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     end_date = (end_date + timedelta(days=1) - timedelta(seconds=1)).strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
-
     try:
         response = requests.get(f"{PRIVACY_API_ENDPOINT}transactions?begin={begin_date}&end={end_date}&page=1&page_size={PRIVACY_PAGE_SIZE}", headers=headers)
-        response.raise_for_status() # Ensure valid response
+        response.raise_for_status()
         data = response.json()
         debug_print(f"Privacy transactions between {begin_date} and {end_date}:\n", data)
         return data["data"]
@@ -59,17 +83,32 @@ def fetch_privacy_transactions(start_date, end_date):
         print(f"Error fetching transactions from Privacy.com: {e}")
         sys.exit(1)
 
-# Find the privacy transaction description based on the ynab_amount from the locally fetched list
 def get_privacy_transaction_details(privacy_amount, privacy_transactions):
+    """
+    Retrieve the merchant descriptor for a Privacy.com transaction based on its amount.
+
+    Args:
+        privacy_amount: Transaction amount to search for.
+        privacy_transactions: List of Privacy.com transactions to search through.
+
+    Returns:
+        Merchant descriptor for the matching transaction or None if not found.
+    """
     for index, txn in enumerate(privacy_transactions):
         if txn["amount"] == privacy_amount:
-            # Remove the transaction from the list and return the descriptor
+            # Remove the matched transaction from the list to prevent multiple matches
             privacy_transactions.pop(index)
             return txn["merchant"]["descriptor"]
     return None
 
-# Update YNAB transaction
 def update_ynab_transaction(transaction_id, memo):
+    """
+    Update the memo of a YNAB transaction.
+
+    Args:
+        transaction_id: ID of the YNAB transaction to update.
+        memo: New memo to set for the transaction.
+    """
     headers = {
         "Authorization": f"Bearer {YNAB_AUTH_TOKEN}",
         "Content-Type": "application/json"
@@ -85,8 +124,8 @@ def update_ynab_transaction(transaction_id, memo):
     except requests.RequestException as e:
         print(f"Error updating YNAB transaction {transaction_id}: {e}")
 
-# Main routine
 def main():
+    """Main routine to update YNAB transactions' memos based on Privacy.com data."""
     ynab_transactions = get_ynab_transactions()
     if not ynab_transactions:
         return
